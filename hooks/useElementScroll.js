2dc1d817ebace435a0e7212f1449b6be6c807994
Reza Erami <reer217@gmail.com>
2020-01-18 17:32:17 +0330

    check hasmore from outside in useElement scroller

--- hooks/useElementScroll.js
+++ hooks/useElementScroll.js
@@ -1,25 +1,19 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
-export default (ref, fn, threshold = 100) => {
-  const doFn = useRef(false);
+export default (ref, fn, hasMore, threshold = 100) => {
   const handleScroll = () => {
     if (
       ref.current.scrollHeight <=
       ref.current.offsetHeight + ref.current.scrollTop + threshold
+      && hasMore
     ) {
-      if (!doFn.current) {
         fn();
-        doFn.current = true;
-      }
-    } else if (doFn.current) {
-      doFn.current = false;
     }
   };
   useEffect(() => {
-    doFn.current = false;
     ref.current.addEventListener('scroll', handleScroll);
     return () => {
       ref.current.removeEventListener('scroll', handleScroll);
     };
-  }, [fn]);
+  }, [hasMore]);
 };
